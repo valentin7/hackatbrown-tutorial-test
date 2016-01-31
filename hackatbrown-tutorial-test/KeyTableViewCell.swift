213d8d8c32570b443ce141aad80ab17cdb0daa27
Valentin Perez <valpe70@gmail.com>
2016-01-31 12:51:59 -0500

    Key Cells

---
+++ hackatbrown-tutorial-test/KeyTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  KeyTableViewCell.swift
+//  hackatbrown-tutorial-test
+//
+//  Created by Valentin Perez on 1/31/16.
+//  Copyright © 2016 Valpe Technologies. All rights reserved.
+//
+
+import UIKit
+
+class KeyTableViewCell: UITableViewCell {
+
+  @IBOutlet var keyLabel: UILabel!
+
+
+  
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
